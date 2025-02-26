849165dbb0b19c32fd862534e7aba457f5c2e660
Syphaar <sifonemmanuel123@gmail.com>
2025-02-26 16:33:19 +0100

    corrected minor mistakes in the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    screens: {
-      "sphones": "320px",
-    },
+    // screens: {
+    //   // "es": "288px",
+    //   // "sphones": "320px",
+    //   "ss": "270px",
+    //   // "mm": "320px",
+    //   "sm": "640px",
+    //   "md": "768px",
+    //   "lg": "1024px",
+    //   "xl": "1280px",
+    //   "2xl": "1536px",
+    // },
     extend: {
-      backgroundImage: {
-        'background-img': "url('/img/bg.jpg')",
-      }
+      // backgroundImage: {
+      //   'background-img': "url('/img/bg.jpg')",
+      // }
     },
   },
   plugins: [],
